60a94edf81f84e43a7642aaf76559e161e116337
SaltfishSheep <76640432+SaltfishSheep@users.noreply.github.com>
2022-12-09 18:21:19 +0800

    Update ServerWorking.java

--- java/saltsheep/dialog/event/ServerWorking.java
+++ java/saltsheep/dialog/event/ServerWorking.java
@@ -1,5 +1,6 @@
 package saltsheep.dialog.event;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -8,6 +9,9 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import saltsheep.dialog.DialogTasks;
+import saltsheep.dialog.base.ADialogChainBase;
+import saltsheep.dialog.base.FDialogHandler;
 import saltsheep.dialog.base.FTaskHandler;
 
 @EventBusSubscriber 
@@ -29,9 +33,22 @@ public class ServerWorking {
 	}
 	
 	@SubscribeEvent
-	public static void onPlayerInteract(PlayerInteractEvent event) {
+	public static void onPlayerInteract(PlayerInteractEvent.EntityInteract event) {
 		if(event.getSide()!=Side.SERVER)
 			return;
+		Entity target = event.getTarget();
+		if(target==null)
+			return;
+		if(DialogTasks.getFileHandler().entityDialogs.containsKey(target.getName())&&FDialogHandler.playersRunningDialog.get(event.getEntityPlayer().getName())==null) {
+			for(String dialog:DialogTasks.getFileHandler().entityDialogs.get(target.getName())) {
+				ADialogChainBase chain = FDialogHandler.createDialog(dialog, (EntityPlayerMP) event.getEntityPlayer());
+				if(chain.canRun()) {
+					chain.runChain();
+					event.setCanceled(true);
+					break;
+				}
+			}
+		}
 	}
 	
 }
