06b298c0e650d6eb5524711ffe39083931b41cea
SaltfishSheep <76640432+SaltfishSheep@users.noreply.github.com>
2022-12-10 09:43:22 +0800

    Update WordSendMessage.java

--- java/saltsheep/dialog/file/word/run/WordSendMessage.java
+++ java/saltsheep/dialog/file/word/run/WordSendMessage.java
@@ -4,38 +4,37 @@ import java.util.Map;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.network.play.server.SPacketChat;
-import net.minecraft.util.text.TextComponentString;
-import saltsheep.dialog.base.Initer;
+import saltsheep.dialog.DialogTasks;
 import saltsheep.dialog.file.IRunAddable;
 import saltsheep.dialog.file.IRunAddable.ExtraRun;
 import saltsheep.dialog.file.word.AFileWordRunHandler;
+import saltsheep.dialog.file.word.run.var.PlaceholderHandler;
 
-public class WordSendMessage extends AFileWordRunHandler {
+public class WordExecuteCommand extends AFileWordRunHandler {
 
 	@Override
 	public String methodWord() {
-		return "sendMessage";
+		return "executeCommand";
 	}
 
 	@Override
 	public String[] paramWords() {
-		return new String[] {"text"};
+		return new String[] {"command"};
 	}
 
 	@Override
 	public ExtraRun getRun(IRunAddable creator, Map<String, String> params) {
-		String text = params.get("text");
-		return new SendMessage(text);
+		String command = params.get("command");
+		return new ExecuteCommand(command);
 	}
 	
-	public static class SendMessage implements ExtraRun{
-		private final String text;
-		public SendMessage(String text) {this.text = text;}
+	public static class ExecuteCommand implements ExtraRun{
+		private final String command;
+		public ExecuteCommand(String command) {this.command = command;}
 		@Override
 		public boolean run(EntityPlayerMP player, EntityLivingBase hostingEntity) {
-			String send = text.replaceAll("%PLAYER%", player.getName());
-			player.connection.sendPacket(new SPacketChat(new TextComponentString(send),Initer.DIALOG));
+			String send = PlaceholderHandler.handler(player, hostingEntity, (command.replaceAll("%PLAYER%", player.getName())));
+			DialogTasks.getMCServer().getCommandManager().executeCommand(DialogTasks.getMCServer(), send);
 			return true;
 		}
 	}
