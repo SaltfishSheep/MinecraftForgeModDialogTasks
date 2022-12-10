51bd8db3dbf9295b6190606cc8901697a4d39beb
SaltfishSheep <76640432+SaltfishSheep@users.noreply.github.com>
2022-12-10 09:43:53 +0800

    Update WordSendMessage.java

--- java/saltsheep/dialog/file/word/run/WordSendMessage.java
+++ java/saltsheep/dialog/file/word/run/WordSendMessage.java
@@ -4,37 +4,39 @@ import java.util.Map;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayerMP;
-import saltsheep.dialog.DialogTasks;
+import net.minecraft.network.play.server.SPacketChat;
+import net.minecraft.util.text.TextComponentString;
+import saltsheep.dialog.base.Initer;
 import saltsheep.dialog.file.IRunAddable;
 import saltsheep.dialog.file.IRunAddable.ExtraRun;
 import saltsheep.dialog.file.word.AFileWordRunHandler;
 import saltsheep.dialog.file.word.run.var.PlaceholderHandler;
 
-public class WordExecuteCommand extends AFileWordRunHandler {
+public class WordSendMessage extends AFileWordRunHandler {
 
 	@Override
 	public String methodWord() {
-		return "executeCommand";
+		return "sendMessage";
 	}
 
 	@Override
 	public String[] paramWords() {
-		return new String[] {"command"};
+		return new String[] {"text"};
 	}
 
 	@Override
 	public ExtraRun getRun(IRunAddable creator, Map<String, String> params) {
-		String command = params.get("command");
-		return new ExecuteCommand(command);
+		String text = params.get("text");
+		return new SendMessage(text);
 	}
 	
-	public static class ExecuteCommand implements ExtraRun{
-		private final String command;
-		public ExecuteCommand(String command) {this.command = command;}
+	public static class SendMessage implements ExtraRun{
+		private final String text;
+		public SendMessage(String text) {this.text = text;}
 		@Override
 		public boolean run(EntityPlayerMP player, EntityLivingBase hostingEntity) {
-			String send = PlaceholderHandler.handler(player, hostingEntity, (command.replaceAll("%PLAYER%", player.getName())));
-			DialogTasks.getMCServer().getCommandManager().executeCommand(DialogTasks.getMCServer(), send);
+			String send = PlaceholderHandler.handler(player, hostingEntity, (text.replaceAll("%PLAYER%", player.getName())));
+			player.connection.sendPacket(new SPacketChat(new TextComponentString(send),Initer.DIALOG));
 			return true;
 		}
 	}
