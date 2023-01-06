20140a0602b7641729bfd784b61372b2b7bf53b6
SaltfishSheep <76640432+SaltfishSheep@users.noreply.github.com>
2023-01-06 16:14:59 +0800

    Delete java/saltsheep/dialog directory

--- java/saltsheep/dialog/base/IDialogCreator.java
+++
@@ -1,9 +0,0 @@
-package saltsheep.dialog.base;
-
-import net.minecraft.entity.player.EntityPlayerMP;
-
-public interface IDialogCreator {
-	
-	public ADialogChainBase create(EntityPlayerMP player) ;
-	
-}
